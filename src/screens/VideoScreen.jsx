ece662c47ce2f9d5d897b267e61c166105088cd1
MohammadJavad14 <mrph14@gamil.com>
2023-03-20 02:16:13 +0330

    styling the current subtitle

--- src/screens/VideoScreen.jsx
+++ src/screens/VideoScreen.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import subs from "../subtitle.json";
+import subsFa from "../subtitle-fa.json";
 
 const VideoScreen = () => {
   const trackRef = useRef(null);
   const videoRef = useRef(null);
   const subRef = {};
+  const [currentSubId, setCurrentSubId] = useState(null);
+  const [trackElement, setTrackElement] = useState(null);
 
   useEffect(() => {
-    trackRef.current.oncuechange = (event) => {
+    setTrackElement(trackRef);
+  }, []);
+
+  if (trackElement)
+    trackElement.current.oncuechange = (event) => {
       let cues = event.target.track.activeCues;
-      if (cues.length > 0 && cues[0].text)
+      if (cues.length > 0 && cues[0].text) {
+        setCurrentSubId(cues[0].id - 1);
         subRef[cues[0].id].scrollIntoView({
           behavior: "smooth",
         });
+      }
     };
-  }, []);
 
   const handleClickSub = (subTime) => {
     videoRef.current.currentTime = subTime;
@@ -23,26 +31,30 @@ const VideoScreen = () => {
 
   return (
     <div className="flex flex-col justify-center items-center overflow-hidden max-h-screen">
-      <div className="md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+      <div className="w-full md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
         <video
-          src="http://localhost:5000/videos/croods_S1E1.mp4"
+          src="http://192.168.1.106:5000/videos/croods_S1E1.mp4"
           controls
           crossOrigin="anonymous"
           ref={videoRef}
         >
           <track
-            src="http://localhost:5000/vtts/croods_S1E1.vtt"
+            src="http://192.168.1.106:5000/vtts/croods_S1E1.vtt"
             default
-            kind="captions"
+            kind="metadata"
             srcLang="en"
             ref={trackRef}
           />
         </video>
       </div>
-      <div className="md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] overflow-y-scroll">
+      <div className="flex flex-col gap-2 py-12 px-3 justify-center items-center w-full md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] bg-dark-1 text-center min-h-[8vh]">
+        <p className="text-white font-bold">{subs[currentSubId]?.text}</p>
+        <p className="text-gold font-semibold">{subsFa[currentSubId]?.text}</p>
+      </div>
+      <div className="w-full md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] overflow-y-scroll bg-dark-2">
         {subs.map((sub, index) => (
           <p
-            className="text-center py-3 cursor-pointer"
+            className="text-center py-3 cursor-pointer text-gray-1"
             key={index}
             onClick={() => handleClickSub(sub.startTime)}
             ref={(ref) => (subRef[index] = ref)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
     colors: {
       white: "#ffffff",
       black: "#000000",
-      gray: { light: "#f5f5f5", DEFAULT: "#6b7280" },
-      dark: "#94a3b8",
+      gray: { light: "#f5f5f5", DEFAULT: "#6b7280", 1: "#7b797c" },
+      dark: {
+        1: "#1a151b",
+        2: "#232027",
+        3: "#1f1c25",
+        4: "#332d39",
+      },
       primary: "#f97316",
+      gold: "#fdeeab",
       error: { DEFAULT: "#dc2626" },
       disabled: { DEFAULT: "#d1d5db" },
       inherit: "inherit",
